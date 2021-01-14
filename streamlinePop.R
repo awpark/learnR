3a5a52ffbe049a67949020b957b1d9f68e4be55e
Andrew Park <andrewpark@Andrews-Mac-mini.local>
2021-01-14 15:21:38 -0500

    recent updates

--- streamlinePop.R
+++ streamlinePop.R
@@ -3,8 +3,10 @@ library(tidyverse)
 library(magrittr)
 pop <- read_csv("https://raw.githubusercontent.com/awpark/learnR/master/pop.csv")
 
-pop %<>% select(-c(state_fips,county_fips,areaname,fipsst,fipsco,region,division,pop19904,pop20104,base20104))
 pop %<>% drop_na(pop1970)
+pop %<>% filter(county_fips>0)
 
-pop2 <- pop
-write_csv(pop2,path="pop2.csv")
+pop %<>% select(-c(state_fips,county_fips,areaname,fipsst,fipsco,region,division,pop19904,pop20104,base20104))
+
+
+write_csv(pop,path="pop.csv")
